55e72d2cce7580849e55ccf6de43ad303f441e89
Hackbright Student <info@hackbrightacademy.com>
2014-06-06 17:09:42 -0700

    All tests pass.

--- list_operations.py
+++ list_operations.py
@@ -18,94 +18,94 @@ operations. The docstring of each function describes what it should do. Consult
 test_list_operations.py for concrete examples of the expected function behavior.
 """
 
-# def head(input_list):
-#     """Return the first element of the input list."""
-#     return input_list[0]
+def head(input_list):
+    """Return the first element of the input list."""
+    return input_list[0]
 
-# def tail(input_list):
-#     """Return all elements of the input list except the first."""
-#     return input_list[1:]
+def tail(input_list):
+    """Return all elements of the input list except the first."""
+    return input_list[1:]
 
-# def last(input_list):
-#     """Return the last element of the input list."""
-#     return input_list[-1]
+def last(input_list):
+    """Return the last element of the input list."""
+    return input_list[-1]
 
-# def init(input_list):
-#     """Return all elements of the input list except the last."""
-#     return input_list[0:-1]
+def init(input_list):
+    """Return all elements of the input list except the last."""
+    return input_list[0:-1]
 
-# def first_three(input_list):
-#     """Return the first three elements of the input list."""
-#     return input_list[0:3]
+def first_three(input_list):
+    """Return the first three elements of the input list."""
+    return input_list[0:3]
 
-# def last_five(input_list):
-#     """Return the last five elements of the input list."""
-#     return input_list[-5:]
+def last_five(input_list):
+    """Return the last five elements of the input list."""
+    return input_list[-5:]
 
-# def middle(input_list):
-#     """Return all elements of the input list except the first two and the last
-#     two.
-#     """
-#     return input_list[2:-2]
+def middle(input_list):
+    """Return all elements of the input list except the first two and the last
+    two.
+    """
+    return input_list[2:-2]
 
-# def inner_four(input_list):
-#     """Return the third, fourth, fifth, and sixth elements of the input list."""
-#     return input_list[2:6]
+def inner_four(input_list):
+    """Return the third, fourth, fifth, and sixth elements of the input list."""
+    return input_list[2:6]
 
-# def inner_four_end(input_list):
-#     """Return the sixth, fifth, fourth, and third elements from the end of the
-#     list, in that order.
-#     """
-#     return input_list[-6:-2:1]
+def inner_four_end(input_list):
+    """Return the sixth, fifth, fourth, and third elements from the end of the
+    list, in that order.
+    """
+    return input_list[-6:-2:1]
 
-# def replace_head(input_list):
-#     """Replace the head of the input list with the value 42."""
-#     input_list[0] = 42
-#     return input_list
+def replace_head(input_list):
+    """Replace the head of the input list with the value 42."""
+    input_list[0] = 42
+    return input_list
 
-#     # List index deletion (del some_list[index])
-#     # (some_list[index] = value)
+    # List index deletion (del some_list[index])
+    # (some_list[index] = value)
 
-# def replace_third_and_last(input_list):
-#     """Replace the third and last elements of the input list with the value 37."""
-#     input_list[2] = 37
-#     input_list[-1] = 37
-#     return input_list
+def replace_third_and_last(input_list):
+    """Replace the third and last elements of the input list with the value 37."""
+    input_list[2] = 37
+    input_list[-1] = 37
+    return input_list
 
-# def replace_middle(input_list):
-#     """Replace all elements of the input list with the the values 42 and 37, in
-#     that order, except for the first two and last two elements.
-#     """
-#     input_list[2:-2] = 42,37
-#     return input_list
+def replace_middle(input_list):
+    """Replace all elements of the input list with the the values 42 and 37, in
+    that order, except for the first two and last two elements.
+    """
+    input_list[2:-2] = 42,37
+    return input_list
 
-# def delete_third_and_seventh(input_list):
-#     """Remove the third and seventh elements of the input list."""
-#     del input_list[6]
-#     del input_list[2]
-#     return input_list
+def delete_third_and_seventh(input_list):
+    """Remove the third and seventh elements of the input list."""
+    del input_list[6]
+    del input_list[2]
+    return input_list
 
-# def delete_middle(input_list):
-#     """Remove all elements from the input list except for the first two and the
-#     last two.
-#     """
-#     del input_list[2:-2]
+def delete_middle(input_list):
+    """Remove all elements from the input list except for the first two and the
+    last two.
+    """
+    del input_list[2:-2]
 
-# """
-# Part 2: Derived operations on lists
-# -----------------------------------
+"""
+Part 2: Derived operations on lists
+-----------------------------------
 
-# In this section you will implement your own versions of the standard list methods.
-# You should use only the primitive operations from Part 1 in your implementations.
-# For loops are also allowed, such as the following:
-#     for element in some_list:
-#         # Do something with element
+In this section you will implement your own versions of the standard list methods.
+You should use only the primitive operations from Part 1 in your implementations.
+For loops are also allowed, such as the following:
+    for element in some_list:
+        # Do something with element
 
-# Each custom method imitates a built-in list method, as described by the docstring
-# for each function. Play with the built-in methods in the Python REPL to get a feel
-# for how they work before trying to write your custom version. You may also look at
-# the test_list_operations.py file for concrete examples of expected behavior.
-# """
+Each custom method imitates a built-in list method, as described by the docstring
+for each function. Play with the built-in methods in the Python REPL to get a feel
+for how they work before trying to write your custom version. You may also look at
+the test_list_operations.py file for concrete examples of expected behavior.
+"""
 
 def custom_len(input_list):
     """custom_len(input_list) imitates len(input_list)"""
@@ -117,8 +117,11 @@ def custom_len(input_list):
 #or the next four functions, get clever using slice operations described in the first half
 def custom_append(input_list, value):
     """custom_append(input_list, value) imitates input_list.append(value)"""
+    # new_list = [value]
+    # input_list = input_list + new_list
+    # return input_list
     new_list = [value]
-    input_list = input_list + new_list
+    input_list[custom_len(input_list):] = new_list
     return input_list
 
 
@@ -172,14 +175,33 @@ def custom_count(input_list, value):
 
 def custom_reverse(input_list):
     """custom_reverse(input_list) imitates input_list.reverse()"""
-    pass
+    input_list[0::] = input_list[::-1]
+
+    # Beth's solution, be wary
+    # new_list = input_list[::-1]
+    # del input_list[:]
+    # input_list += new_list
+    # return input_list
 
 def custom_contains(input_list, value):
     """custom_contains(input_list, value) imitates (value in input_list)"""
-    pass
+    # solution ALSO WORKS
+    # for item in range(custom_len(input_list)):
+    #     if input_list[item] == value:
+    #         return True
+
+    for item in input_list:
+        if item == value:
+            return True
+
+
+    
 
 def custom_equality(some_list, another_list):
     """custom_equality(some_list, another_list) imitates
     (some_list == another_list)
     """
-    pass
+    if some_list == another_list:
+        return True
+    else:
+        return False
